75a2e1736bd6cc58c5f7cb021ac6ce01f97c780c
Arron Archibald <sinchdmitry@gmail.com>
2022-08-11 20:06:53 +0300

    Aug 11 2022 yadro_test_1

--- intro.cpp
+++ intro.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <unistd.h>
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,6 +9,7 @@
 #define END_ERROR       1
 #define SOCKET_ERROR    -1
 #define MAX_SHORT       32767
+#define EMPTY_BUFFER    0
 
 int main() {
     struct addrinfo* addr = NULL;       // информация об IP адресе
@@ -23,7 +25,7 @@ int main() {
                                         Это используется серверными приложениями, 
                                         предназначенными для приема соединений клиентов, 
                                         имеющих любой сетевой адрес. */
-    int result = getaddrinfo(NULL, "80", &hints, &addr);
+    int result = getaddrinfo(NULL, "81", &hints, &addr);
     if (result) {
         std::cout << "error1" << std::endl;
         exit(END_ERROR);
@@ -58,32 +60,38 @@ while (true) {
         char buf[MAX_SHORT];
         result = recv(clientSocket, buf, MAX_SHORT, 0);
 
-        std::cout << result << std::endl;
+        if (result > EMPTY_BUFFER) {
+            std::stringstream response; // сюда будет записываться ответ клиенту
+            std::stringstream response_body;
 
-        std::stringstream response; // сюда будет записываться ответ клиенту
-        std::stringstream response_body;
+            buf[result] = '\0';
 
-        buf[result] = '\0';
+            /* тело ответа (HTML) */
+            response_body << "<title>Test C++ HTTP Server</title>\n"
+                << "<h1>Test page</h1>\n"
+                << "<p>This is body of the test page...</p>\n"
+                << "<h2>Request headers</h2>\n"
+                << "<pre>" << buf << "</pre>\n" 
+                << "<em><small>Test C++ Http Server</small></em>\n";
 
-        /* тело ответа (HTML) */
-        response_body << "<title>Test C++ HTTP Server</title>\n"
-            << "<h1>Test page</h1>\n"
-            << "<p>This is body of the test page...</p>\n"
-            << "<h2>Request headers</h2>\n"
-            << "<pre>" << buf << "</pre>\n"
-            << "<em><small>Test C++ Http Server</small></em>\n";
+            /* весь ответ вместе с заголовками */
+            response << "HTTP/1.1 200 OK\r\n"
+                << "Version: HTTP/1.1\r\n"
+                << "Content-Type: text/html; charset=utf-8\r\n"
+                << "Content-Length: " << response_body.str().length()
+                << "\r\n\r\n"
+                << response_body.str();
 
-        /* весь ответ вместе с заголовками */
-        response << "HTTP/1.1 200 OK\r\n"
-            << "Version: HTTP/1.1\r\n"
-            << "Content-Type: text/html; charset=utf-8\r\n"
-            << "Content-Length: " << response_body.str().length()
-            << "\r\n\r\n"
-            << response_body.str();
+            result = send(clientSocket, response.str().c_str(),
+                response.str().length(), 0);
 
-        result = send(clientSocket, response.str().c_str(),
-            response.str().length(), 0);
-
-        std::cout << result << std::endl;
+            std::cout << result << std::endl;
+        } else if (result == EMPTY_BUFFER) {
+            break;
+        } else if (result == SOCKET_ERROR) {
+            std::cout << "error6" << std::endl;
+        } 
     }
+    close(listning);
+    freeaddrinfo(addr);
 }
